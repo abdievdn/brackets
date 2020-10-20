977c89fd245b1173e1774235f01a02918e8b9d42
Dzmitry_A <abdiev.dn@yandex.by>
2020-10-20 04:32:22 +0300

    feat: task solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    const brackets = Object.fromEntries(bracketsConfig);
+    const buffer = [];
+    
+    for (let i = 0; i < str.length; i++) {
+        if (buffer[0] === str[i]) {
+            buffer.shift();
+        } else if (brackets[str[i]]) {
+            buffer.unshift(brackets[str[i]]);
+        } else {
+            return false;
+        }
+    }
+
+    return buffer.length === 0;
 }
